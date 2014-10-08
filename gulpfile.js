23a79bcdaef0344f391921ed0ef42fa85d9b8d4b
Invalid <wxdong5211@gmail.com>
2014-10-08 14:01:29 +0800

    add gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
 var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var del = require('del');
 
-gulp.task('default', function() {
-    // place code for your default task here
+var paths = {
+    scripts: ['public/js/**/*.coffee', '!public/js/external/**/*.coffee'],
+    images: 'public/img/**/*'
+};
+
+gulp.task('clean', function(cb) {
+    del(['logs/**'],cb);
 });
+
+gulp.task('scripts', ['clean'], function() {
+    // Minify and copy all JavaScript (except vendor scripts)
+    // with sourcemaps all the way down
+    return gulp.src(paths.scripts)
+        .pipe(sourcemaps.init())
+        .pipe(coffee())
+        .pipe(uglify())
+        .pipe(concat('all.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('default', ['scripts']);
